02a57e67e17c0f690c4c3fbfa14ec4bc69b4a452
evelinasii <evevucis@gmail.com>
2016-12-14 13:53:23 +0200

    Created by Coding Ground Client

---
+++ New Project-20161214/main.cpp
@@ -0,0 +1,26 @@
+ #include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    int y; 
+    
+    
+    x= 13;
+    y= 69;
+    
+    cout<<"________________________________________________"     <<endl;                                                
+    cout<<"|Apsveicu tevi tavos  " << x << " Ziemassvetkos          | "   <<endl;     
+    cout<<"|Apsveicu tevi tava " << y << " Jaunaja gada             |"    <<endl;        
+    cout<<"|  *                                            |"<<endl;                                                 
+    cout<<"| ***                                           | "<<endl;
+    cout<<"|*****                                          | "<<endl;
+    cout<<"|  *                                            |"<<endl;
+    cout<<" _______________________________________________ "<<endl;
+                                                                                  cout<<"                                   Evelina Silina "<<endl;
+   
+   return 0;
+}
+
