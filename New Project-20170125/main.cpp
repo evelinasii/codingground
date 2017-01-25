04e68bbc1f93e55b1b33933d43d2f03927968957
evelinasii <evevucis@gmail.com>
2017-01-25 13:27:01 +0200

    Created by Coding Ground Client

---
+++ New Project-20170125/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int y;
+    int x;
+    int rezultats;
+    
+    
+    
+    x= 10;
+    y=20;
+    
+    rezultats= x+y;
+    
+   cout << x << " un " << y << " rezultats ir " << rezultats << endl; 
+   
+   return 0;
+}
+
